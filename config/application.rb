a995c25fc4aa8284601dd78ba5e673e2f3ea01c8
Shrijana <shrijana460@gmail.com>
2016-11-18 10:16:26 +0000

    my first commit

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Workspace
   class Application < Rails::Application
     config.generators do |g|
-  g.helper false
-  g.assets false
+    g.helper false
+    g.assets false
 end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
